227f9e3220daaecb6b53967ac42985055cace3f2
Niran Kalyoncuoglu <nkalyonc@k2m04s07.42kocaeli.com.tr>
2022-03-15 16:32:16 +0300

    %100

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkalyonc <nkalyonc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 14:48:17 by nkalyonc          #+#    #+#             */
+/*   Updated: 2022/03/15 14:50:31 by nkalyonc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_new_left_str(char *left_str);
+char	*ft_search_n_str(int fd, char *left_str);
+char	*ft_get_one_line(char *left_str);
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int ch);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
